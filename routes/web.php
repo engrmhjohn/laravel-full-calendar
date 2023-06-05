507b66d0c054df2634691f2228b79aa642735727
Mehedi Hasan John <111722147+engrmhjohn@users.noreply.github.com>
2023-06-05 11:59:43 +0600

    Laravel Full Calendar

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FullCalenderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('fullcalender', [FullCalenderController::class, 'index']);
+
+Route::post('fullcalenderAjax', [FullCalenderController::class, 'ajax']);
